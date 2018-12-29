9adf53c35a6098823159bf4040a85ae93f44827c
JessicaLyra <jessica.lyra@outlook.com>
2018-12-29 12:07:19 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php 
+	
+//se existir o indice 'entrar' na super global POST
+	if (isset($_POST['entrar'])):
+		$erros = array ();
+		$login = mysqli_escape_string($conecta, $_POST['email']);
+		$senha = mysqli_escape_string($conecta, $_POST['senha']);
+		
+		if (empty($login) or empty($senha)) :
+			$erros [] = "<li>o campo login e senha precisam ser preenchidos</li>";
+			
+		endif;
+	
+	
+	?>
+
 <!DOCTYPE html>
 <html lang ="pt-br">
 	<head>
@@ -8,12 +24,19 @@
 	<body>
 		<div id="corpo-form">
 			<h1>Entrar</h1>
-			<form method="POST" action="processa.php">
+			
+			<?php
+				if (!empty($erros)):
+					foreach($erros as $erro):
+						echo $erro;
+					endforeach;
+				endif;
+			?>
+			<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 				<input type="email" name="email" placeholder="Usuário"></input>
 				<input type="password" name="senha" placeholder="Senha"></input>
-				<input type="submit" value="Acessar"></input>
+				<input type="submit" value="Acessar" name="entrar"></input>
 				<a href="cadastrar.php">Ainda não é inscrito? <strong>Inscreva-se</strong></a>
 			</form>
 		</div>	
-	</body>
-</html>
+	</body></html>
